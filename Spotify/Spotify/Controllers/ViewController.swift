8aaeae016e487ac5da50c870abc0b7f4ff5c24b7
Ibrahim Kebbeh <engineeredlie@gmail.com>
2022-07-05 11:29:04 -0700

    Established MVVM architecture for project layout

---
+++ Spotify/Spotify/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by ikebbeh on 7/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red 
+    }
+
+
+}
+
