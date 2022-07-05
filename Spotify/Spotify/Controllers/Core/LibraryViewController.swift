119fe653185ab5eb54dd84efbf18031cafdfabfa
Ibrahim Kebbeh <engineeredlie@gmail.com>
2022-07-05 13:30:15 -0700

    Home, Search, and Library Views/Controller added along with their titles on their respective views

--- Spotify/Spotify/Controllers/Core/LibraryViewController.swift
+++ Spotify/Spotify/Controllers/Core/LibraryViewController.swift
@@ -11,8 +11,7 @@ class LibraryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
     
 
--- Spotify/Spotify/Controllers/Core/TabBarViewController.swift
+++ Spotify/Spotify/Controllers/Core/TabBarViewController.swift
@@ -12,5 +12,27 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let ViewControllerOne = HomeViewController() 
+        let ViewControllerTwo = SearchViewController()
+        let ViewControllerThree = LibraryViewController()
+        
+        // Establishing title property for each VC
+        ViewControllerOne.title = "Browse"
+        ViewControllerTwo.title = "Search"
+        ViewControllerThree.title = "Library"
+        
+        ViewControllerOne.navigationItem.largeTitleDisplayMode = .always
+        ViewControllerTwo.navigationItem.largeTitleDisplayMode = .always
+        ViewControllerThree.navigationItem.largeTitleDisplayMode = .always
+        
+        let NavControllerOne = UINavigationController(rootViewController: ViewControllerOne)
+        let NavControllerTwo = UINavigationController(rootViewController: ViewControllerTwo)
+        let NavControllerThree = UINavigationController(rootViewController: ViewControllerThree)
+        
+        NavControllerOne.navigationBar.prefersLargeTitles = true
+        NavControllerTwo.navigationBar.prefersLargeTitles = true
+        NavControllerThree.navigationBar.prefersLargeTitles = true
+        
+        setViewControllers([NavControllerOne, NavControllerTwo, NavControllerThree], animated: false)
     }
 }
