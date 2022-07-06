7e030d44d2d75fe72c2baf702ce6fab3708e66fd
Ibrahim Kebbeh <engineeredlie@gmail.com>
2022-07-06 11:06:13 -0700

    client ID and secret for spotify app stored

--- Spotify/Spotify/Managers/AuthManager.swift
+++ Spotify/Spotify/Managers/AuthManager.swift
@@ -12,6 +12,11 @@ final class AuthManager {
     
     private init() {}
     
+    struct Constants {
+        static let clientID = "ffa53eec93614856b4c5b81ecd412261"
+        static let clientSecret = "47326f29745b42c48fa575274f186b03"
+    }
+    
     var isSignedIn: Bool {
         return false
     }
