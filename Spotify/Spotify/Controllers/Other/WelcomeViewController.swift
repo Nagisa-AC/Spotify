bc93017c9f29800853fe1e8ab78e2b17a9802ecc
Ibrahim Kebbeh <engineeredlie@gmail.com>
2022-07-06 12:08:16 -0700

    Sign in button added along with VC to navigate when button is clicked (need to fix coordinates)

--- Spotify/Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Spotify/Controllers/Other/WelcomeViewController.swift
@@ -21,5 +21,18 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         title = "Spotify"
         view.backgroundColor = .systemGreen
+        view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        signInButton.frame = CGRect(x: 30, y: 200, width: 200, height: 50)
+    }
+    
+    @objc func didTapSignIn() {
+        let AuthVC = AuthViewController()
+        AuthVC.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(AuthVC, animated: true)
     }
 }
