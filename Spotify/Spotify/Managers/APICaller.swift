ccf2a2e27d065a0d9ff7e0ff2513326c88e70038
Ibrahim Kebbeh <engineeredlie@gmail.com>
2022-07-13 11:56:43 -0700

    HTTP method enum added

--- Spotify/Spotify/Managers/APICaller.swift
+++ Spotify/Spotify/Managers/APICaller.swift
@@ -13,8 +13,25 @@ final class APICaller {
     private init() {}
     
     public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
-        AuthManager.shared.withValidToken { token in 
+        AuthManager.shared.withValidToken { token in
             
+        }
+    }
+    
+    enum HTTPMethod: String {
+        case GET
+        case POST
+    }
+    
+    // request for user profile
+    private func createRequest(with url: URL?, completion: @escaping (URLRequest) -> Void) {
+        AuthManager.shared.withValidToken { token in
+            guard let apiURL = url else {
+                return
+            }
+            var request = URLRequest(url: apiURL)
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            completion(request)
         }
     }
 }
