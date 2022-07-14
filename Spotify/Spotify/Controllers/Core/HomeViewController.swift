697a5399f5e994a7c561b173863e9e91ef3ec319
Ibrahim Kebbeh <engineeredlie@gmail.com>
2022-07-14 10:41:46 -0700

    calling getNewReleases from HomeViewController

--- Spotify/Spotify/Controllers/Core/HomeViewController.swift
+++ Spotify/Spotify/Controllers/Core/HomeViewController.swift
@@ -15,6 +15,20 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .systemBackground
         title = "Home"
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettings))
+        fetchData()
+    }
+    
+    
+    private func fetchData() {
+        APICaller.shared.getNewReleases { result in
+            switch result {
+            case .success(let model):
+                break
+            case .failure(let error):
+                break;
+            }
+            
+        }
     }
     
     @objc func didTapSettings() {
--- Spotify/Spotify/Managers/APICaller.swift
+++ Spotify/Spotify/Managers/APICaller.swift
@@ -44,7 +44,7 @@ final class APICaller {
     }
     
     public func getNewReleases(completion: @escaping ((Result<String, Error>)) -> Void) {
-        createRequest(with: URL(string: Constants.baseAPIURL + "/browse/new-releases?limit=50"), type: .GET) { request in
+        createRequest(with: URL(string: Constants.baseAPIURL + "/browse/new-releases?limit=1"), type: .GET) { request in
             let task = URLSession.shared.dataTask(with: request) { data, _, error in
                 guard let data = data, error == nil else {
                     completion(.failure(APIError.failedToGetData))
