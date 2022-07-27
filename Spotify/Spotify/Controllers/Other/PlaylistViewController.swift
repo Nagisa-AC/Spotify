cfd42fe1185e3e67cf530b2b3dfbaa36e30fa3a1
Ibrahim Kebbeh <engineeredlie@gmail.com>
2022-07-27 11:01:04 -0700

    cells for songs established

--- Spotify/Spotify/Controllers/Other/PlaylistViewController.swift
+++ Spotify/Spotify/Controllers/Other/PlaylistViewController.swift
@@ -11,6 +11,26 @@ class PlaylistViewController: UIViewController {
 
     private let playlist: Playlist
     
+    private let collectionView = UICollectionView(frame: .zero,
+                                                  collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _, _ -> NSCollectionLayoutSection? in
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)))
+        
+        // breaking cell into subcells
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(80)), subitem: item, count: 1)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+        
+    })
+                                                  )
+    
     init(playlist: Playlist) {
         self.playlist = playlist
         super.init(nibName: nil, bundle: nil)
@@ -25,6 +45,12 @@ class PlaylistViewController: UIViewController {
         title = playlist.name
         view.backgroundColor = .systemBackground
         
+        view.addSubview(collectionView)
+        collectionView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellWithReuseIdentifier: <#T##String#>)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         APICaller.shared.getPlaylistDetails(for: playlist) { result in
             switch result {
             case .success(let model):
@@ -34,5 +60,33 @@ class PlaylistViewController: UIViewController {
             }
         }
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
 
+}
+
+extension PlaylistViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendedTrackCollectionViewCell.identifier, for: indexPath) as? RecommendedTrackCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .red
+        // cell.configure(with: viewModel[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
 }
