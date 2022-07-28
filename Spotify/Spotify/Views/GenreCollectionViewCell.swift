0711dd1cf851ca38e6bbb4ad6fd8b452e04a56da
Ibrahim Kebbeh <engineeredlie@gmail.com>
2022-07-28 11:41:41 -0700

    Genre collection view cell added

---
+++ Spotify/Spotify/Views/GenreCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  GenreCollectionViewCell.swift
+//  Spotify
+//
+//  Created by ikebbeh on 7/28/22.
+//
+
+import UIKit
+
+class GenreCollectionViewCell: UICollectionViewCell {
+    static let identifier = "GenreCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .white
+        imageView.image = UIImage(systemName: "music.quarternote.3", withConfiguration: UIImage.SymbolConfiguration(pointSize: 50, weight: .regular))
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemPurple
+        contentView.addSubview(label)
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
