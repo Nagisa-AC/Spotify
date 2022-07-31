cab86a10ba2e401c6784256db7aaf31340e37500
Ibrahim Kebbeh <engineeredlie@gmail.com>
2022-07-31 11:13:19 -0700

    prototype for controls

---
+++ Spotify/Spotify/Views/PlayerControlsView.swift
@@ -0,0 +1,25 @@
+//
+//  PlayerControlsView.swift
+//  Spotify
+//
+//  Created by ikebbeh on 7/31/22.
+//
+
+import Foundation
+import UIKit
+
+protocol PlayerControlsViewDelegate: AnyObject {
+    func playerControlsViewDidTapPlayPauseButton(_ playerControlsView: PlayerControlsView)
+    func playerControlsViewDidTapForwardButton(_ playerControlsView: PlayerControlsView)
+    func playerControlsViewDidTapBackwardsButton(_ playerControlsView: PlayerControlsView)
+    func playerControlsView(_ playerControlsView: PlayerControlsView, didSlideSlider value: Float)
+}
+
+struct PlayerControlsViewViewModel {
+    let title: String?
+    let subtitle: String?
+}
+
+final class PlayerControlsView: UIView {
+    
+}
